56f4e6b6f8e34be2c1caeffb4e53d59a75fc164c
Evelyn Sun <evelynsun166@gmail.com>
2024-04-17 12:56:23 -0400

    add cta-button component

--- src/pages/WarehousePage/WarehousePage.jsx
+++ src/pages/WarehousePage/WarehousePage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./WarehousePage.scss";
 import WarehouseList from "../../components/WarehouseList/WarehouseList";
 import SearchBox from "../../components/SearchBox/SearchBox";
+import CTAButton from "../../components/CTAButton/CTAButton";
 
 const WarehousePage = () => {
   return (
@@ -9,6 +10,7 @@ const WarehousePage = () => {
       <div className="warehouse-page__page-header">
         <h2>Warehouses</h2>
         <SearchBox />
+        <CTAButton buttonText="+ Add New Warehouse" />
       </div>
       <WarehouseList />
     </div>
