a1b2b45b89fe4690392ec678f8fe38ff5230c187
Evelyn Sun <evelynsun166@gmail.com>
2024-04-17 15:40:44 -0400

    add warehouse list & table components

--- src/components/WarehouseList/WarehouseList.jsx
+++ src/components/WarehouseList/WarehouseList.jsx
@@ -1,21 +1,10 @@
 import "./WarehouseList.scss";
-import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import ListItem from "../ListItem/ListItem";
 import deleteIcon from "../../assets/icons/delete_outline-24px.svg";
 import editIcon from "../../assets/icons/edit-24px.svg";
-import sortIcon from "../../assets/icons/sort-24px.svg";
 
-const WarehouseList = () => {
-  const [warehouses, setWarehouses] = useState();
-  useEffect(() => {
-    const fetchWarehouses = async () => {
-      const response = await axios.get("http://localhost:8080/api/warehouses");
-      setWarehouses(response.data);
-    };
-    fetchWarehouses();
-  }, []);
-
+const WarehouseList = ({ warehouses }) => {
   return (
     warehouses && (
       <>
@@ -57,79 +46,6 @@ const WarehouseList = () => {
             </div>
           );
         })}
-
-        <table className="warehouse-table">
-          <thead>
-            <tr>
-              <th className="label warehouse-table__first-col">
-                warehouse
-                <img
-                  className="warehouse-table__sort-icon"
-                  src={sortIcon}
-                  alt="filter icon"
-                />
-              </th>
-              <th className="label">
-                address
-                <img
-                  className="warehouse-table__sort-icon"
-                  src={sortIcon}
-                  alt="filter icon"
-                />
-              </th>
-              <th className="label">
-                contact name
-                <img
-                  className="warehouse-table__sort-icon"
-                  src={sortIcon}
-                  alt="filter icon"
-                />
-              </th>
-              <th className="label">
-                contact information
-                <img
-                  className="warehouse-table__sort-icon"
-                  src={sortIcon}
-                  alt="filter icon"
-                />
-              </th>
-              <th className="label warehouse-table__last-col">actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {warehouses.map((warehouse) => {
-              return (
-                <tr key={warehouse.id}>
-                  <td className="warehouse-table__first-col">
-                    <ListItem content={warehouse.warehouse_name} link={"/"} />
-                  </td>
-                  <td>
-                    <ListItem
-                      content={`${warehouse.address}, ${warehouse.city}, ${warehouse.country}`}
-                    />
-                  </td>
-                  <td>
-                    <ListItem content={warehouse.contact_name} />
-                  </td>
-                  <td>
-                    <ListItem content={warehouse.contact_phone} />
-                    <ListItem content={warehouse.contact_email} />
-                  </td>
-                  <td className="warehouse-table__last-col">
-                    <div className="warehouse-table__icon-wrapper">
-                      <img
-                        className="warehouse-table__delete-icon"
-                        src={deleteIcon}
-                        alt="delete icon"
-                      />
-                      <img src={editIcon} alt="edit icon" />
-                    </div>
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
       </>
     )
   );
