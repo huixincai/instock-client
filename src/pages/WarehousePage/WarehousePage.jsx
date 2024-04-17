6e1c1830301ea8b69bfcba554b7196727b38eb2a
Evelyn Sun <evelynsun166@gmail.com>
2024-04-17 12:48:23 -0400

    add search-box component

--- src/pages/WarehousePage/WarehousePage.jsx
+++ src/pages/WarehousePage/WarehousePage.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+import "./WarehousePage.scss";
 import WarehouseList from "../../components/WarehouseList/WarehouseList";
+import SearchBox from "../../components/SearchBox/SearchBox";
 
 const WarehousePage = () => {
   return (
-    <>
+    <div className="warehouse-page">
+      <div className="warehouse-page__page-header">
+        <h2>Warehouses</h2>
+        <SearchBox />
+      </div>
       <WarehouseList />
-    </>
+    </div>
   );
 };
 
